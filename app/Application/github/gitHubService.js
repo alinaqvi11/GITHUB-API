0498a8a0143cf9bdc87d565635fb8a9d3bc427dd
alinaqvi11 <93581909+alinaqvi11@users.noreply.github.com>
2022-02-24 18:33:06 +0500

    Initial commit

--- app/Application/github/gitHubService.js
+++ app/Application/github/gitHubService.js
@@ -1,41 +1,39 @@
-const {statusCode,respMessage} = require ("../utils/httpStatus");
-const httpResponse = require ("../utils/httpResponse");
+const { statusCode, respMessage } = require("../utils/httpStatus");
+const httpResponse = require("../utils/httpResponse");
 require("dotenv").config();
 const gitEntity = require("../../domain/entities/gitEntity");
-const gitRepository = require("../../infrastructure/repositories/gitHubRepository")
+const gitRepository = require("../../infrastructure/repositories/gitHubRepository");
 
 class GitHubService {
-  async * getGitHubCommit(perpage,page){
-    try{
-         const commits = await gitRepository.getGitData(perpage,page)
-        yield commits.data;
-    
-  } catch (e) {
-    console.log({error : e.message})
-   }
+  async *getGitHubCommit(perpage, page) {
+    try {
+      const commits = await gitRepository.getGitData(perpage, page);
+      //  console.log(commits.data)
+      yield commits.data;
+    } catch (err) {
+      console.log({ error: err.message });
+    }
   }
- getDataWithGenerator = async (perpage,page) => {
-   try{
-      for await (const value of this.getGitHubCommit(perpage,page)) {     
-      const commit = value.map( item => {
-        const message =  item.commit.message.slice(0,41);
-        const gitObj = {
-          name : item.commit.author.name,
-          email : item.commit.author.email,
-          date : item.commit.author.date,
-          message : message,
-          url : item.url,
-        }
-        return gitEntity.createFromObject(gitObj)
-      })        
-      // console.log(commitData)
-        return httpResponse.create(statusCode.Ok,commit);         
-        }      
-    }catch(e){
-    return httpResponse.create(statusCode.SERVER_ERROR, e);
-   } 
-   };
+  getDataWithGenerator = async (perpage, page) => {
+    try {
+      for await (const value of this.getGitHubCommit(perpage, page)) {
+        const commit = value.map((item) => {
+          const commitMessage = item.commit.message.split("\n\nPR-URL")[0];
+          const gitObj = {
+            name: item.commit.author.name,
+            email: item.commit.author.email,
+            date: item.commit.author.date,
+            message: commitMessage,
+            url: item.url,
+          };
+          return gitEntity.createFromObject(gitObj);
+        });
+        return httpResponse.create(statusCode.Ok, commit);
+      }
+    } catch (err) {
+      return httpResponse.create(statusCode.SERVER_ERROR, err);
+    }
+  };
 }
-
 
 module.exports = new GitHubService();
--- app/Application/utils/httpStatus.js
+++ app/Application/utils/httpStatus.js
@@ -1,10 +1,11 @@
 const statusCode = {
     Ok : 200,  
     SERVER_ERROR : 500 ,
-    Undefined : 404
+    NOT_FOUND : 404
 }
  const respMessage = {
     Success : 'success',
     SERVER_ERROR : 'Internal Server Error',
+    NOT_FOUND : 'Not Found'
 }
 module.exports = {statusCode,respMessage}
